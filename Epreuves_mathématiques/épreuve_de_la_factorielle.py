6e40949808c4abfb010ebfd6e81f63b7b49a3185
Aurel1dr <aurelien.devaux--riviere@efrei.net>
2024-12-19 15:28:29 +0100

    factoriel

--- Epreuves_mathématiques/épreuve_de_la_factorielle.py
+++ Epreuves_mathématiques/épreuve_de_la_factorielle.py
@@ -1,5 +1,22 @@
+import random
+
+
 def factoriel(n):
     factoriel = 1
     for i in range(2,n+1):
         factoriel = factoriel * i
     return factoriel
+
+
+def epreuve_math_factorielle():
+    alea = random.randint(0,10)
+    print("Calculer la factorielle de",alea)
+    x=int(input())
+    if x == factoriel(alea) :
+        print("Correct! vous avez gagné une clé")
+        return True
+    else :
+        return False
+
+print(epreuve_math_factorielle())
+
