f8c16c082b8e1e908ef9ff7534df5ef9fcdaa187
Aurel1dr <aurelien.devaux--riviere@efrei.net>
2024-12-20 21:52:10 +0100

    fc main

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 def jeu():
+    #appeller les fonctions pour centralier actions précédentes pour que le jeu fonctionne
+#demander la composition de l'équipe puis joueur doit prendre décisions au fur et à mesure
+
+
+#annoncer d'abord regle jeu puis création de l'équipe max 3 pers
