a7068307dedd6196a5926149d098f007f0033eb2
Aurel1dr <aurelien.devaux--riviere@efrei.net>
2024-12-30 20:39:12 +0100

    main

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+from enigme_pere_fouras import *
+from epreuve_finale import*
+from epreuves_hasard import*
+from epreuves_logiques import*
+from epreuves_mathematiques import*
+from fonctions_utiles import*
+from data import*
+
 def jeu():
     #appeller les fonctions pour centralier actions précédentes pour que le jeu fonctionne
 #demander la composition de l'équipe puis joueur doit prendre décisions au fur et à mesure
